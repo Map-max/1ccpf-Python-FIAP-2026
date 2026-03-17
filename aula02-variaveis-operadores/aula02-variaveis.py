6355203de75ce68b909217886d5570f22aea9d79
Map-max <matheusapatelli@gmail.com>
2026-03-17 11:41:10 -0300

    aula de Python

---
+++ aula02-variaveis-operadores/aula02-variaveis.py
@@ -0,0 +1,18 @@
+print('hello world')
+
+# Variaveis
+nome =  "Matheus"
+idade = 18
+peso = 50.4
+
+print(nome,idade, peso)
+
+print(f'olá, {nome}!')
+
+# Input
+
+nome = input("digite se nome:")
+idade = int(input("digite sua idade:"))
+peso =  float(input("digite seu peso:"))
+
+print(nome,idade, peso)
---
+++ aula02-variaveis-operadores/desfiao-aula02.py
@@ -0,0 +1,5 @@
+n1 = input('insira o dia de nascimento :' )
+n2 = input('insira o mes de nascimento :' )
+n3 = input('insira o ano de nascimento :' )
+
+print(f"você nasceu em {n1}/{n2}/{n3}")
