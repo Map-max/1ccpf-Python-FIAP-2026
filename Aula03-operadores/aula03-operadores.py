01605564c59dacc938dd44b81b346003bb57cd01
Map-max <matheusapatelli@gmail.com>
2026-03-31 10:49:57 -0300

    continuação da aula

---
+++ Aula03-operadores/aula03-operadores.py
@@ -0,0 +1,22 @@
+num1 = 5
+num2 = 2
+
+print(type(num1), type(num2))
+      
+resultado = num1 + num2
+
+print(resultado)
+
+num1 = num1 + 2
+print(num1)
+
+num1 -= 2
+print(num1)
+
+print(6 < 2)
+
+idade = int(input("sua idade:"))
+
+maior_de_idade = idade >= 18
+
+print(maior_de_idade)
---
+++ Aula03B-modulos/modulos.py
@@ -0,0 +1,5 @@
+import math
+
+num = 4
+raiz = math.sqrt(num)
+print(f"raiz de {num} é {raiz:.2f}")
