c96f25b06ed3cf5ffcdacd1bf4bf2711b7eef67b
Map-max <matheusapatelli@gmail.com>
2026-04-14 10:29:37 -0300

    continuando

--- Aula4/codigo.py
+++ Aula4/codigo.py
@@ -1,14 +1,38 @@
 # Exercicios de repetição
-print("hello")
-# Numero 1 --- ola mundo
 
-while True:
-    print("Hello world")
-    print("deseja repitir o ultimo comando? :")
-    var = input("responda com sim ou não (s ou n):")
-    s = 1
-    n = 0
-    if var == s :
-        continue
-    elif var == n :
-        break
+# Numero 1 --- ola mundo
+def escolha_simples():
+    while True:
+        print("Hello world")
+        print("deseja repitir o ultimo comando?")
+        var = input("responda com sim ou não (s ou n):")
+        if var == "s" :
+            continue
+        elif var == "n" :
+            break
+# Exer num 2 ---
+
+def contagem_ate_cem():
+    num_1 = 0
+    while num_1 <= 100:
+        print(num_1)
+        num_1 += 10
+
+def main():
+    opcoes = {
+            1: escolha_simples,
+            2: contagem_ate_cem
+        }
+
+    print("1 - escolha simples")
+    print("1 - comtagem até cem")
+
+    escolha = int(input("Escolha: "))
+
+    funcao = opcoes.get(escolha)
+
+    if funcao:
+        funcao()
+    else:
+        print("Opção inválida")
+main()
