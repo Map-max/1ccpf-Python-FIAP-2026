0c4229f95cf1d434c510200e892179d3a9398975
Map-max <matheusapatelli@gmail.com>
2026-04-14 10:06:27 -0300

    Parece que o pc da facul me odeia

---
+++ Aula4/codigo.py
@@ -0,0 +1,14 @@
+# Exercicios de repetição
+print("hello")
+# Numero 1 --- ola mundo
+
+while True:
+    print("Hello world")
+    print("deseja repitir o ultimo comando? :")
+    var = input("responda com sim ou não (s ou n):")
+    s = 1
+    n = 0
+    if var == s :
+        continue
+    elif var == n :
+        break
